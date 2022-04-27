0702e4fe44edd649b02f8c6fd7bb58730d313079
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-04-27 01:01:24 -0300

    add time

--- mm.py
+++ mm.py
@@ -2,11 +2,13 @@ import json
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import core
+import sys
 # Falar
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty(
+engine.setProperty(  # Substituir por o nome do arquivo de voz
     'voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_PT-BR_MARIA_11.0')
 
 
@@ -15,6 +17,7 @@ def speak(text):
     engine.runAndWait()
 
 
+# reconhecimento de fala
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 
@@ -22,7 +25,7 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1,
                 rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
-
+# loop do reconhecimento de fala
 while True:
     data = stream.read(8192, exception_on_overflow=False)
     if len(data) == 0:
@@ -30,7 +33,14 @@ while True:
     if rec.AcceptWaveform(data):
         result = rec.Result()
         result = json.loads(result)
+
         if result is not None:
             text = result['text']
+
             print(text)
             speak(text)
+
+            if text == 'que horas são:' or text == 'me diga as horas' or text == 'horas' or text == 'horario' or text=='hora':
+                speak(core.SystemInfo.get_time())
+            if text == 'pare':
+                sys.exit()
